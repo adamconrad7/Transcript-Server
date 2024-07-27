b93adf2f51900d9a4ce20372b801f9742468982f
Adam Conrad <adamconrad720@gmail.com>
2024-07-26 19:15:51 -0700

    housekeeping

--- app/tools/convert_to_onxx.py
+++
@@ -1,7 +0,0 @@
-import nemo.collections.asr as nemo_asr
-
-# Load the Parakeet-CTC model
-model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained("nvidia/parakeet-ctc-0.6b")
-
-# Export to ONNX
-model.export("parakeet_ctc.onnx")
